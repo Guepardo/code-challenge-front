188c745340122bf85249a307e9b2a9c0816bc8e4
Allyson Maciel Guimarães <bsinet@hotmail.com>
2024-07-21 00:42:48 -0300

    feat: implements profiles table skeleton

---
+++ src/pages/profiles-list/profiles-table-skeleton.tsx
@@ -0,0 +1,14 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function ProfilesTableSkeleton() {
+  return (
+    <div className="grid grid-cols-3 gap-3">
+      {[...Array(6)].map((_, index) => (
+        <Skeleton
+          key={index}
+          className="rounded h-64 w-full border"
+        />
+      ))}
+    </div>
+  );
+}
